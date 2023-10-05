a429faf57f7e4ba18217568c6282579a23925521
Andie <134252065+AndieRowell@users.noreply.github.com>
2023-10-05 13:13:03 -0400

    adding pseudocode thoughts

--- js/main.js
+++ js/main.js
@@ -72,6 +72,11 @@ function buildBoard() {
         topRow.classList.add("row");
         mainGridContainer.appendChild(topRow);
 
+//?  test this div!!!!!
+//btnAdd.addEventListener("click", AddNew)
+// add an event listener to the add button - that runs on a click - and calls the addNew function
+//newDiv.classlist.add("div-shadow"); - .div-shadow class in css
+
             const topFirstCol = document.createElement("div");
             topFirstCol.textContent = "0";
             topFirstCol.classList.add("col-4");
@@ -166,6 +171,21 @@ function buildBoard() {
 
 
 // create the array of all of the possible winning combos
+
+const winCombosArr = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [6, 4, 2],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+]
+
+
 // compare against that array to determine if there is a winner everytime a tile is clicked
+function checkWinner(){}
+
 // when the winning combo is determined or tiles run out - a modal is displayed with a winner or tie message
 // a message displays whose turn it is - toggle or switches to the next player turn
