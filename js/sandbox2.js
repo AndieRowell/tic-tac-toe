e48ab8e62be0ca10efc27685089cf5c567d7f743
Andie <134252065+AndieRowell@users.noreply.github.com>
2023-10-06 06:42:54 -0400

    adding init

--- js/sandbox2.js
+++ js/sandbox2.js
@@ -1,1 +1,17 @@
-//initialize my global variables
+//* initialize my global variables
+
+//need to signify the current player = player1 - x starts
+let currentPlayer = "X";
+
+// gameBoard has to have its own array to store markers
+
+
+//connect html div to js
+//const ticTacToe = document.getElementById("tic-tac-toe");
+
+//*initialize the game itself
+
+function initializeGame() {
+    buildBoard();
+    addEventListeners();
+}
